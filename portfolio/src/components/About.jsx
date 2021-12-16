26a4636c040c875e54794e9501e9898447ae8fb2
Ronnie Ross <62971289+ronnieross711@users.noreply.github.com>
2021-12-16 13:29:28 -0600

    fixed typos

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -13,9 +13,9 @@ function About(props) {
                 MongoDB, Mongoose, SQL, Python, Django, and C#.
             </p>
             <p className='second'>     
-                I have a an old hard working mindset and I don't want things in life given to me. I enjoy the pain and 
-                frustration of working hard to succeed. There's nothing like that feeling of success after hours or even
-                days of trial and error. I learned how to take apart and engine from my father and I chose to 
+                I have a an old hard working mindset and I don't want things in life given to me. I enjoy working hard to succeed. 
+                There's nothing like that feeling of success after hours or even
+                days of trial and error. I learned carpentry from my father and I chose to 
                 follow in my father's footsteps and become a carpenter after high school.  
                 I worked for 9 years as a carpenter and then decided to super charge my skills by going to the 
                 Nashville Guitar Craft Academy, where I built and custom designed an
